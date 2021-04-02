fe34da2af412acf2a78b62a04aef7892ac31a47c
Søren Hartmann <soren.hartmann@gmail.com>
2021-04-02 12:42:28 +0200

    inital commit with some data exploration/cleaning

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Final project for course 42186 at DTU',
+    author='Simon Kruse & Søren Hartmann',
+    license='',
+)
